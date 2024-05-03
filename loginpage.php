caf16a4cd05f791483983e50938bab7f90efe79e
aaronf312 <165853189+aaronf312@users.noreply.github.com>
2024-05-02 21:24:15 -0500

    Update loginpage.php
    update login functionality

--- loginpage.php
+++ loginpage.php
@@ -1,39 +1,53 @@
-<!DOCTYPE html>
-<html>
-    <script type="text/javascript" src="js/search.js"></script>
-    <head>
-        <title>Login</title>
-        <link rel ="stylesheet" type="text/CSS" href="recipeCSS.css">
-    </head>
-    <body>
-        
-        <div class="grid">
-            <div class="header">
-                <div class="header-container">
-                    <ul class="styled-list">
-                        <li class="styled-list"><a href="#home">Home</a></li>
-                        <li class="styled-list"><a href="#about">About</a></li>
-                        <li class="styled-list"><a href="#recipes">Recipe Central</a></li>
-                        <li class="styled-list"><input type="text" id="searchInput" placeholder="Search..." /></li>
-                    </ul>
-                </div>
-            </div>
-        
-            <!-- Search results container -->
-            <div id="searchResults"></div>
-        
-            <!-- Link to the external JavaScript file -->
-            <script src="search.js"></script>
-        </div>
-        
-            </div>
-            
-        <div class="flex-container">
-            <form method="post" action="login.php">
-                Name:<input type="text" name="name"><br>
-                Password:<input type="text" name="password"><br>
-                <input type="submit">
-            </form>    
-        </div>
-    </body>
-</html>
+<?php
+
+
+//check if the user is in the database
+function check_user($username, $password){
+    $servername = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname= "user_system";
+    //disable mysqli error report
+    mysqli_report(MYSQLI_REPORT_OFF);
+    // Create connection
+    //i here means improved
+    $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+    if ($conn->connect_error)
+    {
+        $conn->close();
+        return "Database connection error";
+    }
+    
+
+    $sql = "SELECT * FROM users WHERE username = '$username';";
+    $result = $conn->query($sql);
+
+    if (!$result) {
+        $conn->close();
+        return "sql operation error";
+    } 
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        $conn->close();
+        $hashed_password = $result->fetch_assoc()["password"];
+
+        //
+        if(password_verify($password, $hashed_password))
+        {
+            return true;
+        }
+        else
+        {
+            return "The password you entered is wrong!";
+        }
+    }
+    else
+    {
+        $conn->close();
+        return "Cannot match the user name you entered!";
+    }
+}
+
+?>
